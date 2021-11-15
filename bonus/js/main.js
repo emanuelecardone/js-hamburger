50f08aba2dd27d44c4050e887d4455580c88f5c4
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-15 17:33:34 +0100

    Fatta sezione cambio font

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -16,7 +16,7 @@ closeTool.addEventListener('click', function(){
 });
 
 
-// Sezione font
+// SEZIONE EDIT
 
 // Creo la variabile del tasto per aprire il box contenente i vari edit, dei box stessi e del tasto che li chiuderà
 const appearingBoxOpenButton = document.querySelector('.box_open_button');
@@ -35,4 +35,114 @@ appearingBoxCloseButton.addEventListener('click', function(){
     appearingBox.classList.add('d-none');
     appearingBoxCloseButton.classList.add('d-none');
     appearingBoxOpenButton.classList.remove('d-none');    
-})
+});
+
+
+// Sezione font
+
+// Creo la variabile per ogni font marker (usato una variabile per ognuno dato che non riesco a usare il multi-select con class name)
+const editedText1 = document.querySelector('.edited_text_1');
+const editedText2 = document.querySelector('.edited_text_2');
+const editedText3 = document.querySelector('.edited_text_3');
+const editedText4 = document.querySelector('.edited_text_4');
+const arial = document.querySelector('.arial');
+const arialBlack = document.querySelector('.arial-black');
+const verdana = document.querySelector('.verdana');
+const tahoma = document.querySelector('.tahoma');
+const trebuchetMs = document.querySelector('.trebuchet-ms');
+const impact = document.querySelector('.impact');
+const timesNewRoman = document.querySelector('.times-new-roman');
+const didot = document.querySelector('.didot');
+const georgia = document.querySelector('.georgia');
+
+// Cambio font
+arial.addEventListener('click', function(){
+    editedText1.classList.add('arial');
+    editedText2.classList.add('arial');
+    editedText3.classList.add('arial');
+    editedText4.classList.add('arial');
+    editedText1.classList.remove('arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+});
+arialBlack.addEventListener('click', function(){
+    editedText1.classList.add('arial-black');
+    editedText2.classList.add('arial-black');
+    editedText3.classList.add('arial-black');
+    editedText4.classList.add('arial-black');
+    editedText1.classList.remove('arial','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+});
+verdana.addEventListener('click', function(){
+    editedText1.classList.add('verdana');
+    editedText2.classList.add('verdana');
+    editedText3.classList.add('verdana');
+    editedText4.classList.add('verdana');
+    editedText1.classList.remove('arial','arial-black','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial','arial-black','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial','arial-black','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial','arial-black','tahoma','trebuchet-ms','impact','times-new-roman','didot','georgia');
+});
+tahoma.addEventListener('click', function(){
+    editedText1.classList.add('tahoma');
+    editedText2.classList.add('tahoma');
+    editedText3.classList.add('tahoma');
+    editedText4.classList.add('tahoma');
+    editedText1.classList.remove('arial','arial-black','verdana','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial','arial-black','verdana','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial','arial-black','verdana','trebuchet-ms','impact','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial','arial-black','verdana','trebuchet-ms','impact','times-new-roman','didot','georgia');
+});
+trebuchetMs.addEventListener('click', function(){
+    editedText1.classList.add('trebuchet-ms');
+    editedText2.classList.add('trebuchet-ms');
+    editedText3.classList.add('trebuchet-ms');
+    editedText4.classList.add('trebuchet-ms');
+    editedText1.classList.remove('arial','arial-black','verdana','tahoma','impact','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial','arial-black','verdana','tahoma','impact','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial','arial-black','verdana','tahoma','impact','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial','arial-black','verdana','tahoma','impact','times-new-roman','didot','georgia');
+});
+impact.addEventListener('click', function(){
+    editedText1.classList.add('impact');
+    editedText2.classList.add('impact');
+    editedText3.classList.add('impact');
+    editedText4.classList.add('impact');
+    editedText1.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','times-new-roman','didot','georgia');
+    editedText2.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','times-new-roman','didot','georgia');
+    editedText3.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','times-new-roman','didot','georgia');
+    editedText4.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','times-new-roman','didot','georgia');
+});
+timesNewRoman.addEventListener('click', function(){
+    editedText1.classList.add('times-new-roman');
+    editedText2.classList.add('times-new-roman');
+    editedText3.classList.add('times-new-roman');
+    editedText4.classList.add('times-new-roman');
+    editedText1.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','didot','georgia');
+    editedText2.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','didot','georgia');
+    editedText3.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','didot','georgia');
+    editedText4.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','didot','georgia');
+});
+didot.addEventListener('click', function(){
+    editedText1.classList.add('didot');
+    editedText2.classList.add('didot');
+    editedText3.classList.add('didot');
+    editedText4.classList.add('didot');
+    editedText1.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','georgia');
+    editedText2.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','georgia');
+    editedText3.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','georgia');
+    editedText4.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','georgia');
+});
+georgia.addEventListener('click', function(){
+    editedText1.classList.add('georgia');
+    editedText2.classList.add('georgia');
+    editedText3.classList.add('georgia');
+    editedText4.classList.add('georgia');
+    editedText1.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot');
+    editedText2.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot');
+    editedText3.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot');
+    editedText4.classList.remove('arial','arial-black','verdana','tahoma','trebuchet-ms','impact','times-new-roman','didot');
+});
