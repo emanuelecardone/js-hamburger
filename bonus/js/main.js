d8502b3e023df895ea1e2763f5d1b32164547c8c
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-15 16:47:02 +0100

    Impostato paradigma apertura chiusura da js

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -14,3 +14,25 @@ const closeTool = document.querySelector('.hamburger-menu .close');
 closeTool.addEventListener('click', function(){ 
     appearingMenu.classList.remove('active');
 });
+
+
+// Sezione font
+
+// Creo la variabile del tasto per aprire il box contenente i vari edit, dei box stessi e del tasto che li chiuderà
+const appearingBoxOpenButton = document.querySelector('.box_open_button');
+const appearingBox = document.querySelector('.change_box');
+const appearingBoxCloseButton = document.querySelector('.box_close_button');
+
+// Apertura box
+appearingBoxOpenButton.addEventListener('click', function(){
+    appearingBox.classList.remove('d-none');
+    appearingBoxCloseButton.classList.remove('d-none');
+    appearingBoxOpenButton.classList.add('d-none');
+});
+
+// Chiusura box
+appearingBoxCloseButton.addEventListener('click', function(){
+    appearingBox.classList.add('d-none');
+    appearingBoxCloseButton.classList.add('d-none');
+    appearingBoxOpenButton.classList.remove('d-none');    
+})
